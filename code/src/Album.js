fb77826c71d66cf3fff256397000c7569b72b94b
VickBe <32632292+VickBe@users.noreply.github.com>
2020-03-06 13:17:30 +0100

    starded styling

--- code/src/Album.js
+++ code/src/Album.js
@@ -3,11 +3,9 @@ import React from 'react'
 
 export const Album = (props) => {
   console.log(props)
-  return ( <div className="card"> 
-  <img src={props.image} alt="artist"></img>
-  Artist: {props.artistName} 
-  Title: {props.title}
-  </div>
-
-    )
-  }
+  return ( < div className = "card" >
+    <img src = {props.image} alt = "artist"></img>
+    Artist: {props.artistName}
+    Title: {props.title} </div>
+  )
+}
--- code/src/App.js
+++ code/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import data from './data.json'
-import {
-  Album
-} from './Album'
+import { Album } from './Album'
 console.log(data)
 
 export const App = () => {
-    return ( <
-      div > {
-        data.albums.items.map((album) => {
+    return ( <div className="everyCard"> 
+      {data.albums.items.map((album) => {
             return ( < Album key = {album.artists[0].name}
               image = {album.images[1].url}
               artistName = {album.artists[0].name}
