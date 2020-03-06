7e2947e91b076629dbfe8554c94a08e9f7dba5de
VickBe <32632292+VickBe@users.noreply.github.com>
2020-03-06 12:34:34 +0100

    first commit

---
+++ code/src/Album.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export const Album = (props) => {
+  console.log(props)
+  return ( <div className="card"> 
+  <img src={props.image} alt="artist"></img>
+  Artist: {props.artistName} 
+  Title: {props.title}
+  </div>
+
+    )
+  }
